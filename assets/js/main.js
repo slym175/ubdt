1406fa4225cc2ddf82f873a9f2a75c80a4f9dd6b
thuynano <thuyhu9876@gmail.com>
2020-08-31 18:12:00 +0700

    Trang chu

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,4 +14,29 @@ $(document).ready(function () {
             
         }
     })
+
+    $('select.juridical-form-control#sokyhieu').select2({
+        minimumResultsForSearch: Infinity,
+        placeholder: "Số/Ký hiệu",
+    });
+
+    $('select.juridical-form-control#ngaybanhanh').select2({
+        minimumResultsForSearch: Infinity,
+        placeholder: "Ngày ban hành",
+    });
+
+    $('.catalog-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:3
+            }
+        }
+    })
 });
