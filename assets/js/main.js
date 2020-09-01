cc43dab22d88d207f7d676d9e7051828f011deb8
thuynano <thuyhu9876@gmail.com>
2020-09-01 16:44:59 +0700

    1/9 working...

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
         margin:10,
         nav:true,
         dots: false,
+        autoHeight: true,
         items: 1,
         responsive:{
             
@@ -25,17 +26,58 @@ $(document).ready(function () {
         placeholder: "Ngày ban hành",
     });
 
+    $('select.juridical-form-control#tinh').select2({
+        minimumResultsForSearch: Infinity,
+        placeholder: "Tỉnh/Thành phố",
+    });
+
+    $('select.juridical-form-control#nam').select2({
+        minimumResultsForSearch: Infinity,
+        placeholder: "Năm xuất bản",
+    });
+
+    $('select.juridical-form-control#dientich').select2({
+        minimumResultsForSearch: Infinity,
+        placeholder: "Diện tích",
+    });
+
+    $('select.juridical-form-control#loaibieudo').select2({
+        minimumResultsForSearch: Infinity,
+        placeholder: "Loại biểu đồ",
+    });
+
     $('.catalog-carousel').owlCarousel({
         loop: true,
         margin: 20,
         nav: true,
         dots: true,
         responsive:{
-            0:{
+            0: {
                 items:1
             },
-            768:{
-                items:3
+            500: {
+                items:2
+            },
+            768: {
+                items: 3
+            }
+        }
+    })
+
+    $('.statistical-carousel').owlCarousel({
+        loop: true,
+        margin: 15,
+        nav: false,
+        dots: true,
+        responsive:{
+            0: {
+                items:1
+            },
+            500: {
+                items:2
+            },
+            768: {
+                items: 3
             }
         }
     })
